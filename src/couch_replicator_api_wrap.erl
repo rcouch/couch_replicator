32f2b61035464f8cc192b8e2c9ed8c9dc92df69d
benoitc <bchesneau@gmail.com>
2015-07-03 11:21:13 +0200

    uses the correct function

--- src/couch_replicator_api_wrap.erl
+++ src/couch_replicator_api_wrap.erl
@@ -472,8 +472,7 @@ changes_since(Db, Style, StartSeq, UserFun, Options) ->
     },
     QueryParams = get_value(query_params, Options, {[]}),
     Req = changes_json_req(Db, Filter, QueryParams, Options),
-    ChangesFeedFun = couch_httpd_changes:handle_changes(Args, {json_req, Req},
-                                                        Db),
+    ChangesFeedFun = couch_changes:handle_changes(Args, {json_req, Req}, Db),
     ChangesFeedFun(fun({change, Change, _}, _) ->
             UserFun(json_to_doc_info(Change));
         (_, _) ->
